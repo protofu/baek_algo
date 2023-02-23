9074731cf7786456067edba0d2f8a29e692d1e7c
protofu <118592515+protofu@users.noreply.github.com>
2023-02-23 23:11:04 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,22 @@
-def dfs(sta):
-    stack.append(sta)
-    visited[sta] = 1
-    while stack:
-        if not stack:
-            return sum(visited)
-        else:
-            for i in edge[sta]:
-                if not visited[i]:
-                    stack.append(i)
-                    visited[i] = 1
-            sta = stack.pop()
-    return sum(visited)
-
-
 V = int(input())
 E = int(input())
-edge = [[] for _ in range(V+1)]
-stack = []
+node = [[] for _ in range(V+1)]
 visited = [0] * (V+1)
-for _ in range(E):
-    s, e = map(int, input().split())
-    edge[s].append(e)
-    edge[e].append(s)
 
-print(dfs(1)-1)
+for _ in range(E):
+    u, v = map(int, input().split())
+    node[u].append(v)
+    node[v].append(u)
+
+def bfs(S):
+    Q = []
+    Q.append(S)
+    while Q:
+        S = Q.pop(0)
+        for i in node[S]:
+            if not visited[i]:
+                visited[i] = 1
+                Q.append(i)
+    return sum(visited)-1
+
+print(bfs(1))
