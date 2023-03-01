6947f1d9a0c9aa6e2957b4bb036deff71ab34723
protofu <118592515+protofu@users.noreply.github.com>
2023-03-02 02:13:47 +0900

    [Gold V] Title: 주사위 쌓기, Time: 720 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.py
@@ -0,0 +1,30 @@
+n = int(input())
+dice = [list(map(int, input().split())) for _ in range(n)]
+Max_lst = []
+
+for i in range(6):
+    s = i
+    Max = 0
+
+    for j in range(n):
+        if s == 0 or s == 5:
+            Max += max(dice[j][1:5])
+            if j != n-1 and s == 0:
+                s = dice[j+1].index(dice[j][5])
+            elif j != n-1 and s == 5:
+                s = dice[j + 1].index(dice[j][0])
+        elif s == 1 or s == 3:
+            Max += max(dice[j][0], dice[j][2], dice[j][4], dice[j][5])
+            if j != n-1 and s == 1:
+                s = dice[j+1].index(dice[j][3])
+            elif j != n-1 and s == 3:
+                s = dice[j + 1].index(dice[j][1])
+
+        elif s == 2 or s == 4:
+            Max += max(dice[j][0], dice[j][1], dice[j][3], dice[j][5])
+            if j != n-1 and s == 2:
+                s = dice[j+1].index(dice[j][4])
+            elif j != n-1 and s == 4:
+                s = dice[j + 1].index(dice[j][2])
+    Max_lst.append(Max)
+print(max(Max_lst))
