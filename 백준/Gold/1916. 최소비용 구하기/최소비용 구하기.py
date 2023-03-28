b68c0697e57dc1d56cfcaae0e2ab8622041361fd
protofu <118592515+protofu@users.noreply.github.com>
2023-03-28 14:20:33 +0900

    [Gold V] Title: 최소비용 구하기, Time: 348 ms, Memory: 60580 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,44 +1,32 @@
-def get_smallest_node():
-    min_value = INF
-    idx = 1
-    for i in range(1, N+1):
-        if dist[i] < min_value and not visited[i]:
-            min_value = dist[i]
-            idx = i
-    return idx
-
-def dijkstra(start):
-    dist[start] = 0
-    visited[start] = True
-
-    for j in graph[start]:
-        if dist[j[0]] > j[1]:
-            dist[j[0]] = j[1]
-
-    for _ in range(N-1):
-        now = get_smallest_node()
-        visited[now] = True
-
-        for j in graph[now]:
-            cost = dist[now] + j[1]
-            if cost < dist[j[0]] and not visited[j[0]]:
-                dist[j[0]] = cost
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
 
 INF = int(1e9)
 N = int(input())
 M = int(input())
-graph = [[] for _ in range(N+1)]
-dist = [INF for _ in range(N+1)]
-visited = [False]*(N+1)
+graph = [[] for _ in range(N + 1)]
+dist = [INF for _ in range(N + 1)]
 
-for _ in range(M):
+
+for i in range(M):
     start, end, cost = map(int, input().split())
-    graph[start].append((end, cost))
+    graph[start].append([end, cost])
 
 start, end = map(int, input().split())
 
+def dijkstra(start):
+    dist[start] = 0
+    heap = []
+    heappush(heap, [0, start])
+    while heap:
+        cost, start = heappop(heap)
+        if dist[start] < cost:
+            continue
+        for end_node, distance in graph[start]:
+            real_cost = cost + distance
+            if dist[end_node] > real_cost:
+                dist[end_node] = real_cost
+                heappush(heap, [real_cost, end_node])
 dijkstra(start)
-if end == start:
-    print(0)
-else:
-    print(dist[end])
+print(dist[end])
