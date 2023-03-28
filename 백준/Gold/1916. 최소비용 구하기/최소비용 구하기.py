f0920275984cc2fc4307e5ff8319100c51d0438f
protofu <118592515+protofu@users.noreply.github.com>
2023-03-28 12:09:29 +0900

    [Gold V] Title: 최소비용 구하기, Time: 272 ms, Memory: 124624 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,28 +1,39 @@
-import sys
-from heapq import heappush, heappop
-input = sys.stdin.readline
+INF = 100000001
+n = int(input())
+m = int(input())
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+d = [INF for _ in range(n+1)]
+visit = [False for _ in range(n+1)]
 
-N = int(input())
-M = int(input())
-inf = 100000000
-lst = [[] for i in range(N + 1)]
-hq = [inf for i in range(N + 1)]
-for i in range(M):
-    Q, W, E = map(int, input().split())
-    lst[Q].append([W, E])
-start, end = map(int, input().split())
-def dijkstra(start):
-    hq[start] = 0
-    heap = []
-    heappush(heap, [0, start])
-    while heap:
-        E, N = heappop(heap)
-        if hq[N] < E:
-            continue
-        for n_n, wei in lst[N]:
-            n_w = E + wei
-            if hq[n_n] > n_w:
-                hq[n_n] = n_w
-                heappush(heap, [n_w, n_n])
-dijkstra(start)
-print(hq[end])
+
+def minIndex():
+    minValue = INF
+    idx = 0
+    for i in range(1, n+1):
+        if not visit[i] and minValue > d[i]:
+            minValue = d[i]
+            idx = i
+    return idx
+
+
+def dijkstra(x):
+    for i in range(1, n+1):
+        d[i] = graph[x][i]
+    visit[x] = True
+
+    for _ in range(n-2):
+        x = minIndex()
+        visit[x] = True
+        for i in range(1, n+1):
+            if not visit[i] and graph[x][i] + d[x] < d[i]:
+                d[i] = graph[x][i] + d[x]
+    print(d[e])
+
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+s, e = map(int, input().split())
+
+dijkstra(s)
