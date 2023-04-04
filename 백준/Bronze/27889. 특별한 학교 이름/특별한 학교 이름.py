d8c10a6130a3a167986d36901d5413fed7a09e24
protofu <118592515+protofu@users.noreply.github.com>
2023-04-04 16:49:06 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+w = input()
+
+if w == 'NLCS':
+    print('North London Collegiate School')
+elif w == 'BHA':
+    print('Branksome Hall Asia')
+elif w == 'KIS':
+    print('Korea International School')
+else:
+    print('St. Johnsbury Academy')
