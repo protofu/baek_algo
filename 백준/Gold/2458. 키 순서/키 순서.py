71f065eca2421578cb1d4614e938779b2bfe614a
protofu <118592515+protofu@users.noreply.github.com>
2023-04-12 17:05:27 +0900

    [Gold IV] Title: 키 순서, Time: 2884 ms, Memory: 118752 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -0,0 +1,28 @@
+N, M =map(int, input().split())
+INF = 1e9
+graph = [[INF]*N for _ in range(N)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a-1][b-1] = 1
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] > graph[i][k]+graph[k][j]:
+                graph[i][j] = 1
+ans = 0
+
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if graph[i][j] == 1:
+            cnt += 1
+        if graph[j][i] == 1:
+            cnt += 1
+    if cnt == N-1:
+        ans += 1
+
+print(ans)
+
+
