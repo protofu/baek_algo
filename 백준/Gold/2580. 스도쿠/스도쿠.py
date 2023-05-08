04a9ee1c4596465cacffa558fdce8c04eb1d6cfa
protofu <118592515+protofu@users.noreply.github.com>
2023-05-08 15:24:01 +0900

    [Gold IV] Title: 스도쿠, Time: 1928 ms, Memory: 124064 KB -BaekjoonHub

---
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+def isPromising(cnt):
+    if cnt == len(zero):
+        for i in sudoku:
+            print(*i)
+        exit()
+    else:
+        y, x = zero[cnt]
+        posiY, posiX = y//3, x//3
+        sandNum = num[:]
+        for i in range(3*posiY, (posiY+1)*3):
+            for j in range(3*posiX, (posiX+1)*3):
+                if sudoku[i][j] in sandNum:
+                    sandNum.remove(sudoku[i][j])
+
+        for i in range(9):
+            if sudoku[y][i] in sandNum:
+                sandNum.remove(sudoku[y][i])
+            if sudoku[i][x] in sandNum:
+                sandNum.remove(sudoku[i][x])
+        for i in sandNum:
+            sudoku[y][x] = i
+            isPromising(cnt+1)
+        sudoku[y][x] = 0
+
+
+sudoku = [list(map(int, input().split())) for _ in range(9)]
+num = [i for i in range(1, 10)]
+zero = [(j, i) for j in range(9) for i in range(9) if not sudoku[j][i]]
+
+isPromising(0)
