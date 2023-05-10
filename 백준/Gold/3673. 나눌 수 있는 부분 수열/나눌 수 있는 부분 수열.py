577105b0ae9255735cd208d27b517df266f5770b
protofu <118592515+protofu@users.noreply.github.com>
2023-05-10 14:20:38 +0900

    [Gold III] Title: 나눌 수 있는 부분 수열, Time: 1728 ms, Memory: 57652 KB -BaekjoonHub

--- 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.py
+++ 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.py
@@ -1,12 +1,13 @@
 for _ in range(int(input())):
     d, n = map(int, input().split())
-    numbers = list(map(lambda x: int(x)%d, input().split())) # 0, 1, 2, 3 으로
+    a = list(map(int, input().split()))
+    s = [0] * (n + 1)
+    for i in range(1, n + 1):
+        s[i] = (s[i - 1] + a[i - 1]) % d
+    cnt = [0] * d
+    for i in range(n + 1):
+        cnt[s[i]] += 1
     ans = 0
-    visited = [1] + [0] * (d-1)
-    for num in numbers:
-        ans = (ans + num) % d
-        visited[ans] += 1
-    result = 0
-    for i in range(d):
-         result += visited[i] * (visited[i] - 1) // 2
-    print(result)
+    for x in range(d):
+        ans += cnt[x] * (cnt[x] - 1) // 2
+    print(ans)
