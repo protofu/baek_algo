2aefe608b6882096f6ee90da142b0107a0c5da5f
protofu <118592515+protofu@users.noreply.github.com>
2023-05-10 14:01:17 +0900

    [Gold III] Title: 나눌 수 있는 부분 수열, Time: 1736 ms, Memory: 63596 KB -BaekjoonHub

--- 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.py
+++ 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.py
@@ -2,7 +2,7 @@ for _ in range(int(input())):
     d, n = map(int, input().split())
     numbers = list(map(lambda x: int(x)%d, input().split())) # 0, 1, 2, 3 으로
     ans = 0
-    visited = [1] + [0] * 1000000
+    visited = [1] + [0] * (d-1)
     for num in numbers:
         ans = (ans + num) % d
         visited[ans] += 1
