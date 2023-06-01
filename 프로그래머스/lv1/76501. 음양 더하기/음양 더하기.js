f2e1d763527c81adc47779a999bbdde7d1dd6be5
protofu <118592515+protofu@users.noreply.github.com>
2023-06-01 23:22:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,14 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    
+    for(let i=0; i<signs.length; i++){
+        if(signs[i] == false){
+            answer -= absolutes[i]
+        }
+        else{
+            answer += absolutes[i]
+        }
+    }
+    
+    return answer;
+}
