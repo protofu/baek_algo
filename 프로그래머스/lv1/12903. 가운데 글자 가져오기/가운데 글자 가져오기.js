b4667f3dae940167e3c4947966149480cb3a1178
protofu <118592515+protofu@users.noreply.github.com>
2023-06-01 23:47:08 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    let answer = '';
+    
+    if(s.length%2){
+        // 홀수
+        answer += s[(s.length-1)/2]
+    }
+    else{
+        answer += s[s.length/2 -1] + s[s.length/2]
+    }
+    
+    return answer;
+}
