0ed5b4d0faa1f9132f0765ed313537e0f6ce866c
protofu <118592515+protofu@users.noreply.github.com>
2023-06-13 18:51:31 +0900

    [Gold II] Title: 1의 개수 세기, Time: 48 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Gold/9527. 1의 개수 세기/1의 개수 세기.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+sys.setrecursionlimit(10 ** 8)
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def sum_f(x):
+    if x <= 0:
+        return 0
+
+    seung = int(math.log2(x))
+    floor_2pow = 2 ** seung
+    if floor_2pow == x:
+        return seung * x // 2 + 1
+
+    diff = x - floor_2pow
+    return sum_f(floor_2pow) + diff + sum_f(diff)
+
+
+a, b = map(int, input().split())
+print(sum_f(b) - sum_f(a - 1))
