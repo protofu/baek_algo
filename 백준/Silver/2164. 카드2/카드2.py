778e1e8fa5ce7ec0362fe299e334ff55c529a22a
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-06-30 15:00:06 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 55044 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,10 +1,12 @@
 from collections import deque
 
 n = int(input())
-
+# 앞뒤로 입출력 위해 deque로 생성
 q = deque([i for i in range(n, 0, -1)])
-
+# 반복문 카드가 1개 남을때 까지
 while len(q) != 1:
+    # 맨위 첫장 빼고
     q.pop()
+    # 그 다음장 뺀걸 가장 밑으로
     q.appendleft(q.pop())
 print(q[0])
