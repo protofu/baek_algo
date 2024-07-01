32d24f2b5475f6a4d194f2a71e4c0d6bcc241ae4
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-01 19:44:56 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,19 +1,15 @@
-import sys
-input = sys.stdin.readline
-
-N, K = map(int, input().split())
-
-coins = [int(input()) for _ in range(N)]
-ans = 0
-sel_coin = []
-for coin in coins:
-    if coin <= K:
-       sel_coin.append(coin)
-    else:
-        break
-for i in sel_coin[::-1]:
-    ans += K//i
-    K %= i
-    if not K:
-        break
-print(ans)
+# n 동전 종류 개수, k 만들어야할 금액
+n, k = map(int, input().split())
+# 동전 종류 리스트로 받기
+coins = [int(input()) for _ in range(n)]
+# 최대한 적게 쓰기
+# 큰 금액부터 채우기
+use_cnt = 0
+for idx in range(n-1, -1, -1):
+    # 사용할 동전보다 가치가 크거나 같다면
+    if k >= coins[idx]:
+        # 총 금액을 동전 가치로 나눈 몫 = 사용할 코인 개수
+        use_cnt += k//coins[idx]
+        # 사용한 만큼 금액에서 빼주기
+        k %= coins[idx]
+print(use_cnt)
