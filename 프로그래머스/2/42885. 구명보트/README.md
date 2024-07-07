26b16bb5c27c2312609ba16e066b1fb6ca19b9b1
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-07 20:09:14 +0900

    [level 2] Title: 구명보트, Time: 6.93 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 7.86 ms
+메모리: 10.6 MB, 시간: 6.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 07일 20:00:10
+2024년 07월 07일 20:09:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -4,15 +4,16 @@ def solution(people, limit):
     # 무게 낮은 값부터 정렬
     people.sort()
     l = len(people)
-    start = 0
-    end = l-1
-    # 모두 보트를 탈때까지
-    while start <= end:
+    start = 0       # 시작 포인트
+    end = l-1       # 끝 포인트
+    # 모두 보트를 탈때까지 투포인터를 돌려보겠쉼다
+    # 2명 탄것만 셀거라서 = 은 안붙임
+    while start < end:
+        # 처음과 끝 2명이 된다면
         if people[start] + people[end] <= limit:
             start += 1
-            end -= 1
-        else:
-            end -= 1
-        answer += 1
-        
-    return answer
+            answer += 1
+        # 어찌되든 끝점은 줄어야함 혼자 타거나 같이 타거나
+        end -= 1
+    # 같이 탄 보트만 answer에 넣었으니 총 인원에서 빼주면 된다.
+    return l - answer
