b8c65891fd38a265e8317184c20f41d43e7292ec
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-09 22:55:59 +0900

    Update 체육복.py

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -6,20 +6,23 @@ def solution(n, lost, reserve):
     # 체육복 챙겨넣기
     for i in range(1, n+1):
         if i in reserve:
-            visited[i] +=1
+            visited[i] += 1
         if i in lost:
             visited[i] -= 1
         
-    print(visited)  
     # 1번 학생부터 순차로
     for i in range(1, n+1):
+        # 체육복이 없는 경우
         if visited[i] == 0:
+            # 앞사람 먼저 찾아보기
             if visited[i-1] == 2:
                 visited[i] += 1
                 visited[i-1] -= 1
+            # 그다음 뒷사람
             elif visited[i+1] ==2:
                 visited[i] += 1
                 visited[i+1] -= 1
+        # 모든 처리 후 0 초과면 +1
         if visited[i] > 0:
             answer+= 1
             
