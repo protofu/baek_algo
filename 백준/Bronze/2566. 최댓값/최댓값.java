ad406c9ef5fe5c15e85a9640c696cb18ef54c58d
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-13 16:25:56 +0900

    [Bronze III] Title: 최댓값, Time: 196 ms, Memory: 18464 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int maxNum = 0, x = 0, y = 0;
+		int maxNum = 0, x = 1, y = 1;
 	
 		for (int i=0;i<9;i++) {
 			for (int j=0;j<9;j++) {
 				int n = sc.nextInt();
-				if (maxNum <= n) {
+				if (maxNum < n) {
 					maxNum = n;
 					x = i+1;
 					y = j+1;
