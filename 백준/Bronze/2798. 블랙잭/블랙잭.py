19fa0747b06910998ae522a6d04290bdbc1bc626
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-13 21:01:52 +0900

    [Bronze II] Title: 블랙잭, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,13 @@
-a,b=map(int,input().split())
-cards = list(map(int,input().split()))
-sum_=[]
-last_sum=[]
-for i in cards:
-    for j in range(cards.index(i)+1,a):
-        for k in range(j+1,a):
-            sum_.append(i+cards[j]+cards[k])
-for i in sum_:
-    if i<=b:
-        last_sum.append(i)           
-print(max(last_sum))
+n, m = map(int, input().split())
+cards = list(map(int ,input().split()))
+# 합을 -1로 해두기
+ans = -1
+# 카드 3장이라 쉽게 생각 3중 for, N이 100이하라 100만번 안에 끝남
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            sum_cards = cards[i]+cards[j]+cards[k]
+            # m보다 합이 작거나 같고, 합이 원래 저장된 합보다 크면 초기화
+            if sum_cards <= m and sum_cards > ans:
+                ans = sum_cards
+print(ans)
