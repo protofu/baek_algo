dfb76f6297ff73c60e8ff6617ef279438ddc7147
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-14 00:54:50 +0900

    [Silver II] Title: 마인크래프트, Time: 460 ms, Memory: 128900 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,28 +1,34 @@
-import sys
-input = sys.stdin.readline
-
-N, M, B = map(int, input().split())
-ground = sum([list(map(int, input().split())) for _ in range(N)], [])
-end = max(ground)
-key = []
-time = 256*250000
+n, m, inven = map(int, input().split())
+mine = sum([list(map(int, input().split())) for _ in range(n)], [])
+mine_high = max(mine)
+mine_low = min(mine)
+time = float('inf')
 lend = 0
-for g in range(0, end+1):
-    dig = fill = now_t = 0
-    for i in ground:
-        if g > i:
-            fill += g - i
-        elif g < i:
-            dig += i - g
-        else:
-            pass
-    if B < fill-dig:
-        pass
-    else:
-        now_t = dig*2 + fill
-        if time >= now_t:
-            time = now_t
-            lend = g
+# 좌표 (i, j)의 가장 위에 있는 블록을 제거하여 인벤토리에 넣는다. -> 2초
+# 인벤토리에서 블록 하나를 꺼내어 좌표 (i, j)의 가장 위에 있는 블록 위에 놓는다. -> 1초
+# 대충 땅크기 250*250, 땅최대 0-256, 1610만회 -> 1초 안걸림 0.8초쯤?
+for standard in range(mine_low, mine_high+1):
+    dig = fill = tmp_time = 0
+    for ground in mine:
+        # 기준보다 낮은 땅이면
+        if ground < standard:
+            # 얼마나 채워야할지 측정
+            fill += standard - ground
+        # 기준보다 높은 땅이면
+        elif ground > standard:
+            # 얼마나 파야할지 측정
+            dig += ground - standard
 
+    # 기준에 대해 모두 측정 후 판단
+
+    # 채워야 할 블록 갯수가 inven에 있는것 보다 많다면 그냥 통과
+    # 땅을 파면 블록이 생기니까 fill - dig
+    if inven >= fill - dig:
+        # 해당 기준에 대한 시간 측정
+        tmp_time = (fill + dig*2)
+        # 원래 시간보다 덜 걸린다면 초기화
+        if tmp_time <= time:
+            time = tmp_time
+            lend = standard
 
 print(time, lend)
