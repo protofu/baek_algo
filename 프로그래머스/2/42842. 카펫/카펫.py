ad2bfb6dfa0fab2809d24ea2bd5028153f1d227e
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-14 19:01:32 +0900

    [level 2] Title: 카펫, Time: 28.88 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,15 +1,16 @@
 def solution(brown, yellow):
     answer = []
     xy = []
+    # 전체 블록 수
     blocks = brown + yellow
+    # 가능한 x, y 조합 찾기
     for i in range(3, blocks//3+1):
+        # 가능한 x, y 중 가로(x)가 큰것만
         if blocks%i == 0 and i >= blocks//i:
-            xy.append([i, blocks//i])
-    
-    for x, y in xy:
-        if (x-2)*(y-2) == yellow:
-            answer.append(x)
-            answer.append(y)
-    
-    print(answer)
+            # 조합 중 x-2 * y-2 가 yellow인것만 골라내기
+            if (i-2)*(blocks//i-2) == yellow:
+                answer.append(i)
+                answer.append(blocks//i)
+                break
+
     return answer
