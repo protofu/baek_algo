3108f084ca75d51e0689dc1d70e456262555007f
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-14 19:46:39 +0900

    [level 2] Title: 타겟 넘버, Time: 406.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,17 @@
 def solution(numbers, target):
     answer = 0
-    n = len(numbers)
-    
-    def dfs(idx, rlt):
-        if idx == n:
-            if rlt == target:
-                nonlocal answer
+    # 재귀 dfs 탐색
+    def dfs(idx, num):
+        # 인덱스가 초과할때까지 ex) 4번인덱스 까지 있으면 5번에서 결과 도출할 수 있도록
+        if idx == len(numbers):
+            # 타겟 넘버와 같다면 +1
+            if num == target:
+                nonlocal answer 
                 answer += 1
             return
-        else:
-            dfs(idx+1, rlt+numbers[idx])
-            dfs(idx+1, rlt-numbers[idx])
+        # 더하고 빼는 동작으로 재귀 2개 생성
+        dfs(idx+1, num+numbers[idx])
+        dfs(idx+1, num-numbers[idx])
+    # 초기값으로 시작
     dfs(0, 0)
     return answer
