fda74ad39430b95287140e19775e1e9cc1637b47
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-22 23:41:46 +0900

    [Silver II] Title: 유기농 배추, Time: 312 ms, Memory: 31500 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,26 +1,39 @@
 import sys
 sys.setrecursionlimit(10**6)
-for test_case in range(int(input())):
-    M, N, E = map(int, input().split())
-    node = [[0]*M for _ in range(N)]
-    dy = [-1, 1, 0, 0]
-    dx = [0, 0, -1, 1]
-    cnt = 0
-    for _ in range(E):
+
+# 범위 췤!
+def is_out(y, x):
+    return 0>y or 0>x or col<=y or row <=x
+# 4방향 췤!
+dir = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+def dfs(y, x):
+    # 탐색한번 해주고
+    for dy, dx in dir:
+        ny, nx = y+dy, x+dx
+        # 벗어나거나 배추가 아니거나 이미 초토화된 곳이면 넘어갑니다.
+        if is_out(ny, nx) or baechu[ny][nx] != 1:
+            continue
+        # 초토화시 -1로!
+        baechu[ny][nx] = -1
+        dfs(ny, nx)
+
+
+for _ in range(int(input())):
+    row, col, k = map(int, input().split())
+    ans = 0
+    # 배추밭 컴온!
+    baechu = [[0]*row for _ in range(col)]
+    # 배추를 심자!
+    for _ in range(k):
         x, y = map(int, input().split())
-        node[y][x] = 1
-
-    def dfs(y, x):
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-            if N > ny >= 0 and M > nx >= 0:
-                if node[ny][nx] == 1:
-                    node[ny][nx] = 0
-                    dfs(ny, nx)
-
-    for y in range(N):
-        for x in range(M):
-            if node[y][x] == 1:
+        baechu[y][x] = 1
+    # 배추밭 본격 초토화 작전 시작
+    for y in range(col):
+        for x in range(row):
+            # 배추가 있으면 냠냠
+            if baechu[y][x] == 1:
+                baechu[y][x] = -1
                 dfs(y, x)
-                cnt += 1
-    print(cnt)
+                # 한뭉텅이 초토화 성공
+                ans+=1
+    print(ans)
