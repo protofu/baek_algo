8fbd94397fc437254120f15abbcb4b3262b2cf67
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-22 13:43:11 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.03 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,13 +1,17 @@
 class Solution {
     public int[] solution(int[] emergency) {
         int len = emergency.length;
+        // 순서 배열
         int[] answer = new int[len];
+        // 원래 값 배열
         int[] before = new int[len];
-        
+
+        // 원래값 채워넣기
         for (int i = 0; i<len;i++){
             before[i] = emergency[i];
         }
         
+        // 내림차순 정렬 
         for (int i = 0;i<len-1;i++){
             for (int j = i+1;j<len;j++){
                 if (emergency[i] < emergency[j]){
@@ -17,6 +21,8 @@ class Solution {
                 }
             }
         }
+
+        // 원래값을 탐색하며 등수 집어넣기 (등수 = 내림차순 배열의 인덱스+1)
         for(int i=0;i<len;i++){
             for(int j = 0;j<len; j++){
                 if (emergency[i] == before[j]){
