d6568672ff8caef0c2728ce693d5f128df27689b
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-23 12:34:01 +0900

    [Silver II] Title: 섬의 개수, Time: 108 ms, Memory: 31468 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,29 +1,37 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
 
-dx = [-1, -1, -1, 0, 0, 1, 1, 1]
-dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+# 범위 췤!
+def is_out(y, x):
+    return 0>y or 0>x or h<=y or w<=x
+# 8방향 췤!
+dir = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+def dfs(y, x):
+    for dy, dx in dir:
+        ny, nx = y+dy, x+dx
+        # 범위 밖 or 1이 아니라면 넘기기.
+        if is_out(ny, nx) or island[ny][nx] != 1: continue
+        island[ny][nx] = -1
+        dfs(ny, nx)
 
-def bfs(y, x, mat):
-    queue = deque()
-    queue.append([y, x])
-    while queue:
-        y, x = queue.popleft()
-        for i in range(8):
-            ny, nx = y + dy[i], x + dx[i]
-            if 0 <= nx < w and 0 <= ny < h and mat[ny][nx]:
-                queue.append([ny, nx])
-                mat[ny][nx] = 0
 
-while True:
+
+while(True):
     w, h = map(int, input().split())
-    if not w and not h:
-        break
-    mat = [list(map(int, input().split())) for _ in range(h)]
+    # 탈출조건 넣어주기!
+    if (w==0 or h==0): break
+    # 웰컴 아일랜드!
+    island = [list(map(int, input().split())) for _ in range(h)]
     ans = 0
+    # 완탐!
     for y in range(h):
         for x in range(w):
-            if mat[y][x]:
-                mat[y][x] = 0
-                bfs(y, x, mat)
-                ans += 1
+            # 콜럼버스 빙의해서 신대륙 발견하기 작전 실행
+            if island[y][x] == 1:
+                dfs(y, x)
+                ans+=1
     print(ans)
+
+
+
+
