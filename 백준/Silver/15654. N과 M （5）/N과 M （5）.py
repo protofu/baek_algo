c8220056ef8b7e1beeb137bc6fc9e9408b2aa72c
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-27 18:30:39 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,22 +1,19 @@
-import sys
-input = sys.stdin.readline
-
-N, M = map(int, input().split())
-num_lst = sorted(list(map(int, input().split())))
-visited = [False]*N
-lst = []
-
-def dfs():
-    if len(lst) == M:
-        print(*lst)
+# 백트래킹
+def back():
+    # 수열의 길이가 조건에 맞으면 출력
+    if len(ans) == m:
+        print(*ans)
         return
-    for i in range(N):
-        if not visited[i]:
-            lst.append(num_lst[i])
-            visited[i] = True
-            dfs()
-            lst.pop()
-            visited[i] = False
+    # 순회하며 ans에 넣기
+    for i in lst:
+        if i in ans: continue
+        ans.append(i)
+        back()
+        ans.pop()
 
-
-dfs()
+n, m = map(int, input().split())
+# 입력과 오름차순 정렬
+lst = sorted(list(map(int, input().split())))
+ans = []
+# 백트래킹 시작
+back()
