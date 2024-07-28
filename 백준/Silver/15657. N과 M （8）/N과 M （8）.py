a22aeecc36b140dc423af2f0e3092c824cbe8362
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-07-28 19:47:31 +0900

    [Silver III] Title: N과 M (8), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,17 +1,18 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-
-N, M = map(int, input().split())
-num_lst = sorted(list(map(int, input().split())))
-lst = []
-def dfs(S):
-    if len(lst) == M:
-        print(*lst)
+# 백트래킹
+def back(start):
+    # m개가 되면 도출
+    if len(ans) == m:
+        print(*ans)
         return
-    else:
-        for i in range(S, N):
-            lst.append(num_lst[i])
-            dfs(i)
-            lst.pop()
-dfs(0)
+    # 백트래킹 로직
+    # 이전 숫자만 넣지 않으면 문제없기에 start 인덱스 지정
+    for i in range(start, n):
+        ans.append(lst[i])
+        back(i)
+        ans.pop()
+
+n, m = map(int, input().split())
+# 입력받으면서 오름차순 정렬
+lst = sorted(list(map(int, input().split())))
+ans = []
+back(0)
