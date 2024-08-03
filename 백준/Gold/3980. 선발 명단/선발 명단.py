9247a115ad72fbab82899bcb6346483d14bae9f9
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-08-03 19:54:55 +0900

    [Gold V] Title: 선발 명단, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/3980. 선발 명단/선발 명단.py
+++ 백준/Gold/3980. 선발 명단/선발 명단.py
@@ -1,21 +1,21 @@
-def dfs(score, cnt):
-    global flag, ans
-    if cnt == 11:
-        ans = max(ans, score)
+def back(p_score, posi):
+    global ans
+    # 종료 조건 -> posi가 11까지 갔단건 모든 포지션을 고려했단 말
+    if posi == 11:
+        # 원래 포지션스코어와 현재 포지션 스코어중 큰걸로 초기화
+        ans = max(ans, p_score)
         return
-
+    # 11개의 포지션 탐색
     for i in range(11):
-        if pos[cnt][i] and not visited[i]:
-            visited[i] = True
-            dfs(score+pos[cnt][i], cnt+1)
-            visited[i] = False
-
+        # 포지션 스코어가 0이면 or 이미 더해진 포지션 스코어라면 패스
+        if players[posi][i] == 0 or visit[i]: continue
+        visit[i] = True
+        back(p_score+players[posi][i], posi+1)
+        visit[i] = False
 
 for _ in range(int(input())):
-    pos = [[0]*11 for _ in range(11)]
-    visited = [0] * 11
-    flag = ans = 0
-    for i in range(11):
-        pos[i] = list(map(int, input().split()))
-    dfs(0, 0)
+    players = [list(map(int, input().split())) for _ in range(11)]
+    visit = [False] * 11
+    ans = 0
+    back(0, 0)
     print(ans)
